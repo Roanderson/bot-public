b3036a754efa43ddf8a03855c7704b4567b77737
Roanderson <49733565+Roanderson@users.noreply.github.com>
2019-10-25 11:31:11 -0300

    Update orioninvest.py

--- orioninvest.py
+++ orioninvest.py
@@ -9,19 +9,19 @@ from database import db, insert_wallet, user_exist, insert_deposito, insert_saqu
 db.bind(provider='sqlite', filename='database.sqlite')
 db.generate_mapping()
 
-
+#API DO BOT COM TELEGRAM
 bot = telebot.TeleBot(conts.API_TOKEN)
 
 
 x=2000.00000000
 Salcon=--(-x+x)#+(0.5/100)
 
-
+#INICIO DO BOT/ BOTÕES E DATABASE
 @bot.message_handler(commands=['start'])
 def handle_start (message):
     if not user_exist(message.from_user.id):
         create_user(message.from_user.id, message.chat.id, message.from_user.first_name, carteira="")
-
+    #BOTÕES DO BOT
     user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
     user_markup.row(' NST  {:.8f} '.format(Salcon))
     user_markup.row('Iniciar', 'Deposito')
@@ -30,7 +30,7 @@ def handle_start (message):
     bot.send_message(message.from_user.id, 'Seu cadastro foi criado com sucesso! \n'
                                            ' Seja, Bem vindo {} ! ao melhor Bot de investimento.'.format(message.from_user.first_name), reply_markup=user_markup)
 
-
+#COMANDO DA CARTEIRA
 @bot.message_handler(content_types=["text"])
 def wallet(message, user_markup=None):
     if message.text == 'Wallet NST':
@@ -47,10 +47,11 @@ def add_wallet(msg):
     insert_wallet(msg.from_user.id, carteira)
     bot.reply_to(msg, "Carteira adicionada com sucesso!")
 
-    
+
+#COMANDO DA CONTA DO CLIENTE   
 @bot.message_handler(commands=['Iniciar'])
 def inicializar(message):
-    bot.send_message(message.from_user.id, """"SUA CONTA""" '\n'
+    bot.send_message(message.from_user.id, """"CONTA DO CLIENTE""" '\n'
                     '\n'
                     """ Saldo em conta """ '\n'  # valor de rendimento diario
                     """ NST:""" + str(float(Salcon)) + '\n' +  # (saldo_investido*0.5/100)aumente 0.5% todos dias apos 24hrs com base no valor investido
@@ -63,6 +64,7 @@ def inicializar(message):
 
     
 
+#COMANDO DE DEPOSITO
 @bot.message_handler(content_types=["text"])
 def deposito (message, user_markup=None):
     if message.text == 'Deposito':
@@ -70,13 +72,17 @@ def deposito (message, user_markup=None):
         bot.send_message(message.from_user.id, 'Qual o valor do seu deposito, lembrando que o minimo é de 2000 NST.')
         total_deposito = message.text
         insert_deposito(msg.from_user.id, total_deposito)
-        sleep(1)
+         """if message.text == total_deposito >= (conts.minimo_deposito):
+                bot.send_message(message.from_user.id, 'O SEU DEPOSITO FOI REALIZADO COM SUCESSO!.')
+            else:
+                bot.send_message(message.from_user.id, 'O VALOR PARA DEPOSITO NÃO É PERMITIDO. ')"""
         bot.send_message(message.from_user.id, 'Por favor aguarde um minuto estamos gerando sua carteira : ')
         sleep(2)
         bot.send_message(message.from_user.id, '0x96036e644b9968adb299e12f8ef3acf157ab8bb1')
 
 
 
+#COMANDO DE SAQUE
 @bot.message_handler(content_types=["text"])
 def saque(message, user_markup=None):
     if message.text == 'Saque':
@@ -88,10 +94,15 @@ Por favor certifique-se que está a utilizar o contrato inteligente correto ou o
 (LEMBRANDO O MINIMO PARA SAQUE É DE 1500NST) """)
         total_saque = message.text
         insert_saque(msg.from_user.id, total_saque)
+           """if message.text == str(Salcon) >= (conts.minimo_saque):
+                bot.send_message(message.from_user.id, 'O SEU SAQUE FOI SOLICITADO COM SUCESSO!!! , \n PRAZO DE PAGAMENTO DE ÁTE D+05 ÚTEIS.')
+            else:
+                bot.send_message(message.from_user.id, 'O valor de saquer é menor que o permetido. ')"""
         sleep(1)
         bot.send_message(message.from_user.id,'OBRIGADO POR CONFIAR NA ORION INVESTIMENTO VIEMOS COM PRINCIPAL OBJETIVO AJUDA A TODOS VOCÊS.')
 
-        
+
+#COMANDO DE SUPORTE E AJUDA    
 @bot.message_handler(content_types=["text"])
 def suporte (message, user_markup=None):
     if message.text == 'Suporte':
