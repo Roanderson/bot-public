925aff06239dae33ac639907e9647a78e1fbabb4
Roanderson <49733565+Roanderson@users.noreply.github.com>
2019-10-25 11:17:31 -0300

    Update orioninvest.py

--- orioninvest.py
+++ orioninvest.py
@@ -1,6 +1,13 @@
-import telebot
+import import telebot
+
 from time import sleep
 import conts
+
+from database import db, insert_wallet, user_exist, insert_deposito, insert_saque, create_user
+
+#INSTANCIA MAPEAMENTO SEM CRIAR TABELAS
+db.bind(provider='sqlite', filename='database.sqlite')
+db.generate_mapping()
 
 
 bot = telebot.TeleBot(conts.API_TOKEN)
@@ -12,35 +19,82 @@ Salcon=--(-x+x)#+(0.5/100)
 
 @bot.message_handler(commands=['start'])
 def handle_start (message):
+    if not user_exist(message.from_user.id):
+        create_user(message.from_user.id, message.chat.id, message.from_user.first_name, carteira="")
+
     user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
     user_markup.row(' NST  {:.8f} '.format(Salcon))
     user_markup.row('Iniciar', 'Deposito')
-    user_markup.row('Wallet NST', 'Saque', 'Suporte' )
+    user_markup.row('Wallet NST', 'Saque', 'Suporte')
     user_markup.row('Ajuda')
-    bot.send_message(message.from_user.id, 'Seja, Bem vindo {} ! ao melhor Bot de investimento.'.format(message.from_user.username), reply_markup=user_markup)
+    bot.send_message(message.from_user.id, 'Seu cadastro foi criado com sucesso! \n'
+                                           ' Seja, Bem vindo {} ! ao melhor Bot de investimento.'.format(message.from_user.first_name), reply_markup=user_markup)
+
 
 @bot.message_handler(content_types=["text"])
-def handle_text (message, user_markup=None):
-    if message.text == 'Iniciar':
-        bot.send_message(message.from_user.id, """"SUA CONTA""" '\n'
-                        '\n'
-                        """ Saldo em conta """ '\n'    #valor de rendimento diario
-                        """ NST:"""+str(float(Salcon))+'\n'+ #(saldo_investido*0.5/100)aumente 0.5% todos dias apos 24hrs com base no valor investido
-                        '\n'
-                        """ Saldo Investido"""'\n'   #deposito
-                        """ NST:"""+str(float(x))+'\n'    #quantidade de moedas depositadas
-                        '\n'
-                        """ Saldo Sacado """ '\n'  
-                        """ NST: 0.00000000 """)
+def wallet(message, user_markup=None):
+    if message.text == 'Wallet NST':
+        sent=bot.send_message(message.chat.id, """Cripto moeda é um token Ethereum.
+O endereço do token do contrato inteligente de NST é o seguinte:
+(0xD89040Ac9823B72F64d71f66Fa2DeAE7C8520671).
+Por favor certifique-se que está a utilizar o contrato inteligente correto ou os seus fundos poderão perder-se irremediavelmente! \n
+INFORME A SUA CARTEIRA NST(NEW SOLUTION ) :""")
+        bot.register_next_step_handler(sent, add_wallet)
 
-    elif message.text == 'Deposito':
-        bot.send_message(message.from_user.id, """A Cripto moeda é um token Ethereum.\n Por favor, transfira para este endereço apenas moeda NST. Depósitos de outras moedas neste endereço perderão!\n O valor mínimo para depósito é 2000 NST.\n O endereço do token do contrato inteligente de NST é o seguinte :\n 0xD89040Ac9823B72F64d71f66Fa2DeAE7C8520671.\n Por favor certifique-se que está a utilizar o contrato inteligente correto ou os seus fundos poderão perder-se irremediavelme""")
-        bot.send_message(message.from_user.id, 'Por favor aguarde um minuto estamos gerando sua cartira : ')
+def add_wallet(msg):
+    carteira = (msg.text)
+    carteira = carteira.replace(' ', '')
+    insert_wallet(msg.from_user.id, carteira)
+    bot.reply_to(msg, "Carteira adicionada com sucesso!")
+
+    
+@bot.message_handler(commands=['Iniciar'])
+def inicializar(message):
+    bot.send_message(message.from_user.id, """"SUA CONTA""" '\n'
+                    '\n'
+                    """ Saldo em conta """ '\n'  # valor de rendimento diario
+                    """ NST:""" + str(float(Salcon)) + '\n' +  # (saldo_investido*0.5/100)aumente 0.5% todos dias apos 24hrs com base no valor investido
+                    '\n'
+                    """ Saldo Investido"""'\n'  # deposito
+                    """ NST:""" + str(float(x)) + '\n'  # quantidade de moedas depositadas
+                    '\n'
+                    """ Saldo Sacado """ '\n'
+                    """ NST: 0.00000000 """)
+
+    
+
+@bot.message_handler(content_types=["text"])
+def deposito (message, user_markup=None):
+    if message.text == 'Deposito':
+        bot.send_message(message.from_user.id,"""A Cripto moeda é um token Ethereum.\n Por favor, transfira para este endereço apenas moeda NST. Depósitos de outras moedas neste endereço perderão!\n O valor mínimo para depósito é 2000 NST.\n O endereço do token do contrato inteligente de NST é o seguinte :\n 0xD89040Ac9823B72F64d71f66Fa2DeAE7C8520671.\n Por favor certifique-se que está a utilizar o contrato inteligente correto ou os seus fundos poderão perder-se irremediavelme""")
+        bot.send_message(message.from_user.id, 'Qual o valor do seu deposito, lembrando que o minimo é de 2000 NST.')
+        total_deposito = message.text
+        insert_deposito(msg.from_user.id, total_deposito)
+        sleep(1)
+        bot.send_message(message.from_user.id, 'Por favor aguarde um minuto estamos gerando sua carteira : ')
         sleep(2)
         bot.send_message(message.from_user.id, '0x96036e644b9968adb299e12f8ef3acf157ab8bb1')
 
 
-    elif message.text == 'Suporte':
+
+@bot.message_handler(content_types=["text"])
+def saque(message, user_markup=None):
+    if message.text == 'Saque':
+        bot.send_message(message.from_user.id, """Cripto moeda é um token Ethereum.
+O endereço do token do contrato inteligente de NST é o seguinte:
+(0xD89040Ac9823B72F64d71f66Fa2DeAE7C8520671).
+Por favor certifique-se que está a utilizar o contrato inteligente correto ou os seus fundos poderão perder-se irremediavelmente! """)
+        bot.send_message(message.from_user.id,""" INFORME A QUANTIDADE DE NST :(valores inteiros)
+(LEMBRANDO O MINIMO PARA SAQUE É DE 1500NST) """)
+        total_saque = message.text
+        insert_saque(msg.from_user.id, total_saque)
+        sleep(1)
+        bot.send_message(message.from_user.id,'OBRIGADO POR CONFIAR NA ORION INVESTIMENTO VIEMOS COM PRINCIPAL OBJETIVO AJUDA A TODOS VOCÊS.')
+
+        
+@bot.message_handler(content_types=["text"])
+def suporte (message, user_markup=None):
+    if message.text == 'Suporte':
         bot.send_message(message.from_user.id,""" Agora você tem contato direto com o nosso suporte.
 Basta você ta enviando um email para
 (suporte.orion@gmail.com),
@@ -89,37 +143,6 @@ caso não faça o envio da hash por esquecer ou não saber entrar em contato atr
 Tenho mais duvida como posso ta sabendo mais sobre Orion Investimento?\n
 Entre em contato com nos atraves do nosso email                        
 (suporte.orion@gmail.com), mande sua pergunta que estaremos respondendo o mais rapido possivel.""")
-
-    elif message.text == 'Saque':
-        bot.send_message(message.from_user.id, """Cripto moeda é um token Ethereum.
-O endereço do token do contrato inteligente de NST é o seguinte:
-(0xD89040Ac9823B72F64d71f66Fa2DeAE7C8520671).
-Por favor certifique-se que está a utilizar o contrato inteligente correto ou os seus fundos poderão perder-se irremediavelmente! """)
-        bot.send_message(message.from_user.id,""" INFORME A QUANTIDADE DE NST :(valores inteiros)
-(LEMBRANDO O MINIMO PARA SAQUE É DE 1500NST) """)
-        """if message.text == str(Salcon) < '1500':
-            bot.send_message(message.from_user.id, 'O SEU SAQUE FOI SOLICITADO COM SUCESSO!!! , \n PRAZO DE PAGAMENTO DE ÁTE D+05 ÚTEIS.')
-        else:
-            bot.send_message(message.from_user.id, 'O valor de saquer é menor que o permetido. ')"""
-        sleep(1)
-        bot.send_message(message.from_user.id,'OBRIGADO POR CONFIAR NA ORION INVESTIMENTO VIEMOS COM PRINCIPAL OBJETIVO AJUDA A TODOS VOCÊS.')
-
-#bot.message_handler(func=True)
-"""def wallet(message):
-    if message.text == 'Wallet NST':
-        sent=bot.send_message(message.chat.id, Cripto moeda é um token Ethereum.
-    O endereço do token do contrato inteligente de NST é o seguinte:
-    (0xD89040Ac9823B72F64d71f66Fa2DeAE7C8520671).
-    Por favor certifique-se que está a utilizar o contrato inteligente correto ou os seus fundos poderão perder-se irremediavelmente! \n
-    INFORME A SUA CARTEIRA NST(NEW SOLUTION ) :)
-        bot.register_next_step_handler(sent, salva)
-
-def salva(message):
-    arquivo=open('oriondb.txt', 'w')
-    arquivo.write(int(message.chat.id)  + message.chat.username + message.chat.text )
-    bot.send_message(message.chat.id, 'CARTEIRA SALVA COM SUCESSO!')
-    bot.register_next_step_handler( handle_text)
-"""
 
 
 print('em execução...')
